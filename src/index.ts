cf28b0cbd2a194e074e80f3d06018f17d0b55829
DukeBas <35384117+DukeBas@users.noreply.github.com>
2021-07-12 15:55:24 +0200

    Canvas as background

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,13 @@ const sketch = (p: p5) => {
   p.preload = () => { };
 
   p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    const canvas = p.createCanvas(p.windowWidth, p.windowHeight);
     console.log("Hello World!");
+
+    canvas.position(0, 0);  // make canvas start in top-left corner
+    canvas.style('z-index', '-1');  // set canvas as background
+    p.frameRate(60);  // target framerate
+
   };
 
   p.windowResized = () => {
