cabab5418e9223e5814c7dd856c47168efff4ed4
DukeBas <35384117+DukeBas@users.noreply.github.com>
2021-07-12 15:47:16 +0200

    Working p5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
+import * as p5 from 'p5';
+
 console.log("Hello World!");
+
+const sketch = (p: p5) => {
+
+  p.preload = () => {
+
+  };
+
+
+  p.setup = () => {
+
+    p.createCanvas(p.windowWidth, p.windowHeight);
+
+  };
+
+
+  p.windowResized = () => {
+
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+
+  };
+
+
+  p.draw = () => {
+
+    p.background(100);
+
+    p.ellipse(100, 150, 180, 60);
+
+  }
+};
+
+
+const sketchP = new p5(sketch);
