4f9de4e59291f7be272639614b9b39f0f1490452
DukeBas <35384117+DukeBas@users.noreply.github.com>
2021-07-12 15:54:32 +0200

    Sidebar start

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,21 @@
 import * as p5 from 'p5';
 
-console.log("Hello World!");
-
 const sketch = (p: p5) => {
 
-  p.preload = () => {
-
-  };
-
+  p.preload = () => { };
 
   p.setup = () => {
-
     p.createCanvas(p.windowWidth, p.windowHeight);
-
+    console.log("Hello World!");
   };
-
 
   p.windowResized = () => {
-
     p.resizeCanvas(p.windowWidth, p.windowHeight);
-
   };
 
-
   p.draw = () => {
-
     p.background(100);
-
     p.ellipse(100, 150, 180, 60);
-
   }
 };
 
